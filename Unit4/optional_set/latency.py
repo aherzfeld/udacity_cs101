9a2426162ef992d1e82069b3587c4bc14ffaf6c1
Andrew <herzfeld.andrew@gmail.com>
2018-04-23 10:36:32 +0700

    finished latency.py

--- Unit4/optional_set/latency.py
+++ Unit4/optional_set/latency.py
@@ -5,9 +5,14 @@
 
 speed_of_light = 300000. # km per second
 
-def speed_fraction():
-
-
+def speed_fraction(time, distance):
+#inputs are result of a traceroute in ms & km
+	time_secs = time / 1000.0
+	
+	speed = 2* distance / time_secs
+	
+	return speed / 300000.0
+	
 
 print speed_fraction(50,5000)
 #>>> 0.666666666667
