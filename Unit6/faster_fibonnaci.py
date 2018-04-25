c7f0a8d0cf53f3c52ac3189e8421d8cfad31d7e1
Andrew <herzfeld.andrew@gmail.com>
2018-04-25 17:01:51 +0700

    improved procedure

--- Unit6/faster_fibonnaci.py
+++ Unit6/faster_fibonnaci.py
@@ -2,21 +2,11 @@
 #fibonacci(36).
 
 def fibonacci(n):
-	n1 = 1
-	n0 = 0
-	fib = 1
-	i = 1
-	if n == 0:
-		return 0
-	if n == 1:
-		return 1
-	else:
-		while i < n:
-			fib = n1 + n0
-			n0 = n1
-			n1 = fib
-			i = i + 1
-		return fib
+	current = 0
+	next = 1
+	for i in range(0,n):
+	    current, after = after, current + after
+	return current
 
 print fibonacci(5)
 
